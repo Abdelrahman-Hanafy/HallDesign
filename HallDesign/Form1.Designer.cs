61b6184c0b335caa86a459276cfda76e36c20409
Abdelrahman Mohamed Mostafa <a.m.mostafa1@student.aast.edu>
2022-08-03 13:25:57 +0200

    Add project files.

---
+++ HallDesign/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace HallDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.finish = new System.Windows.Forms.Button();
+            this.colorBalet = new System.Windows.Forms.Panel();
+            this.black = new System.Windows.Forms.PictureBox();
+            this.yellow = new System.Windows.Forms.PictureBox();
+            this.red = new System.Windows.Forms.PictureBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.cols = new System.Windows.Forms.TextBox();
+            this.rows = new System.Windows.Forms.TextBox();
+            this.drawChairs = new System.Windows.Forms.Button();
+            this.colorBalet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.black)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Location = new System.Drawing.Point(1, 46);
+            this.canvas.Margin = new System.Windows.Forms.Padding(5);
+            this.canvas.Name = "canvas";
+            this.canvas.Padding = new System.Windows.Forms.Padding(5);
+            this.canvas.Size = new System.Drawing.Size(980, 533);
+            this.canvas.TabIndex = 0;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // finish
+            // 
+            this.finish.Location = new System.Drawing.Point(97, 7);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(75, 33);
+            this.finish.TabIndex = 1;
+            this.finish.Text = "Finish";
+            this.finish.UseVisualStyleBackColor = true;
+            this.finish.Click += new System.EventHandler(this.finish_Click);
+            // 
+            // colorBalet
+            // 
+            this.colorBalet.Controls.Add(this.black);
+            this.colorBalet.Controls.Add(this.yellow);
+            this.colorBalet.Controls.Add(this.red);
+            this.colorBalet.Location = new System.Drawing.Point(12, 12);
+            this.colorBalet.Name = "colorBalet";
+            this.colorBalet.Size = new System.Drawing.Size(79, 28);
+            this.colorBalet.TabIndex = 0;
+            // 
+            // black
+            // 
+            this.black.BackColor = System.Drawing.Color.Black;
+            this.black.Location = new System.Drawing.Point(55, 3);
+            this.black.Name = "black";
+            this.black.Size = new System.Drawing.Size(20, 20);
+            this.black.TabIndex = 3;
+            this.black.TabStop = false;
+            this.black.Click += new System.EventHandler(this.color_Click);
+            // 
+            // yellow
+            // 
+            this.yellow.BackColor = System.Drawing.Color.Yellow;
+            this.yellow.Location = new System.Drawing.Point(29, 3);
+            this.yellow.Name = "yellow";
+            this.yellow.Size = new System.Drawing.Size(20, 20);
+            this.yellow.TabIndex = 2;
+            this.yellow.TabStop = false;
+            this.yellow.Click += new System.EventHandler(this.color_Click);
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.Red;
+            this.red.Location = new System.Drawing.Point(3, 3);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(20, 20);
+            this.red.TabIndex = 1;
+            this.red.TabStop = false;
+            this.red.Click += new System.EventHandler(this.color_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(178, 7);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 33);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // cols
+            // 
+            this.cols.Enabled = false;
+            this.cols.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.cols.Location = new System.Drawing.Point(424, 12);
+            this.cols.Name = "cols";
+            this.cols.Size = new System.Drawing.Size(147, 26);
+            this.cols.TabIndex = 3;
+            this.cols.Text = "Num of Columns";
+            this.cols.Enter += new System.EventHandler(this.cols_Enter);
+            this.cols.Leave += new System.EventHandler(this.cols_Leave);
+            // 
+            // rows
+            // 
+            this.rows.Enabled = false;
+            this.rows.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.rows.Location = new System.Drawing.Point(577, 10);
+            this.rows.Name = "rows";
+            this.rows.Size = new System.Drawing.Size(147, 26);
+            this.rows.TabIndex = 4;
+            this.rows.Text = "Num of Rows";
+            this.rows.Enter += new System.EventHandler(this.rows_Enter);
+            this.rows.Leave += new System.EventHandler(this.rows_Leave);
+            // 
+            // drawChairs
+            // 
+            this.drawChairs.Enabled = false;
+            this.drawChairs.Location = new System.Drawing.Point(730, 7);
+            this.drawChairs.Name = "drawChairs";
+            this.drawChairs.Size = new System.Drawing.Size(75, 30);
+            this.drawChairs.TabIndex = 5;
+            this.drawChairs.Text = "Set";
+            this.drawChairs.UseVisualStyleBackColor = true;
+            this.drawChairs.Click += new System.EventHandler(this.drawChairs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 584);
+            this.Controls.Add(this.drawChairs);
+            this.Controls.Add(this.rows);
+            this.Controls.Add(this.cols);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.finish);
+            this.Controls.Add(this.colorBalet);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Hall Design";
+            this.colorBalet.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.black)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.red)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Panel colorBalet;
+        private System.Windows.Forms.PictureBox black;
+        private System.Windows.Forms.PictureBox yellow;
+        private System.Windows.Forms.PictureBox red;
+        private System.Windows.Forms.Button finish;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox cols;
+        private System.Windows.Forms.TextBox rows;
+        private System.Windows.Forms.Button drawChairs;
+    }
+}
+
---
+++ HallDesign/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HallDesign
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen curser;
+
+        bool curserMoving = false;
+        bool painting = true;
+
+        Point st;
+
+        Rectangle current;
+        Rectangle selected;
+        List<KeyValuePair<Color,Rectangle>> rectangles;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = canvas.CreateGraphics();
+            curser = new Pen(Color.Black,5);
+            rectangles = new List<KeyValuePair<Color, Rectangle>>();
+        }
+
+        private void color_Click(object sender, EventArgs e)
+        {
+            PictureBox color = (PictureBox)sender;
+            curser.Color = color.BackColor;
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            curserMoving = true;
+            st = e.Location;
+
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            curserMoving = false;
+            
+            foreach (KeyValuePair<Color, Rectangle> rect in rectangles)
+            {
+                if (isOverlap(rect.Value))
+                {
+                    MessageBox.Show("Can not add this rectangle as it overlap with drawn one!!");
+                    updateScreen();
+                    return;
+                }
+            }
+            rectangles.Add(new KeyValuePair<Color, Rectangle>(curser.Color,current));
+
+        }
+
+        private bool isOverlap(Rectangle r)
+        {
+
+            //bool flg = r.Contains(current.Top,current.Left) || r.Contains(current.Top, current.Right) || r.Contains(current.Bottom, current.Left) || r.Contains(current.Bottom, current.Right);
+            //bool flg =  r.Contains(current.Bottom, current.Left) ;
+
+            return !(Math.Min(r.Left, current.Left) > Math.Max(r.Right, current.Right) && // width > 0
+                    Math.Min(r.Top, current.Top) > Math.Max(r.Bottom,current.Bottom));  // height > 0
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (curserMoving && painting)
+            {
+
+                Rectangle r = new Rectangle();
+                r.X = Math.Min(st.X, e.X);
+                r.Y = Math.Min(st.Y, e.Y);
+                r.Width = Math.Abs(st.X - e.X);
+                r.Height = Math.Abs(st.Y - e.Y);
+
+                current = r;
+                drawRect(r);
+
+            }
+        }
+
+        private void finish_Click(object sender, EventArgs e)
+        {
+            updateScreen();
+            painting = false;
+            rows.Enabled = true;
+            cols.Enabled = true;
+        }
+
+        private void updateScreen()
+        {
+            g.Clear(Color.White);
+            foreach (KeyValuePair<Color, Rectangle> rect in rectangles)
+            {
+                curser.Color = rect.Key;
+                drawRect(rect.Value);
+
+            }
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            rectangles.Clear();
+            painting = true;
+        }
+
+        private void drawRect(Rectangle r)
+        {
+            g.DrawRectangle(curser,r);
+
+        }
+
+        private void cols_Enter(object sender, EventArgs e)
+        {
+            if(cols.Text == "Num of Columns")
+            {
+                cols.ForeColor = Color.Black;
+                cols.Text = "";
+            }
+
+        }
+
+        private void rows_Enter(object sender, EventArgs e)
+        {
+
+            if (rows.Text == "Num of Rows")
+            {
+                rows.ForeColor = Color.Black;
+                rows.Text = "";
+            }
+        }
+
+        private void cols_Leave(object sender, EventArgs e)
+        {
+            if (cols.Text == "")
+            {
+                cols.ForeColor = Color.Silver;
+                cols.Text = "Num of Columns";
+            }
+
+        }
+        private void rows_Leave(object sender, EventArgs e)
+        {
+
+            if (rows.Text == "")
+            {
+                rows.ForeColor = Color.Silver;
+                rows.Text = "Num of Rows";
+            }
+        }
+
+        private void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!painting)
+            {
+                drawChairs.Enabled = true;
+                Point click = e.Location;
+                foreach (KeyValuePair<Color, Rectangle> rect in rectangles)
+                {
+                    if (rect.Value.Contains(click))
+                    {
+                        selected = rect.Value;
+                        finish_Click(sender,e);
+                        g.DrawRectangle(Pens.Blue, Rectangle.Round(selected));
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void drawChairs_Click(object sender, EventArgs e)
+        {
+            TextRenderer.DrawText(g, $"X:{cols.Text},Y:{rows.Text}", new Font("Arial", 12, FontStyle.Bold), selected, Color.Red);
+        }
+    }
+}
